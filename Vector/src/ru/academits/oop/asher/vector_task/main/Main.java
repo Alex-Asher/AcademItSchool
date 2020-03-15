0687286a853f6c731be0996ca73ad06808b8499d
Alex-Asher <AA07101994@gmail.com>
2020-03-15 20:50:47 +0700

    Изменены имена некоторых методов и переменных. Изменена логика конструкторов. Удалены методы get/setComponents. Значительно сокращены и доработаны методы для получения суммы и разности. Методы multiplyByScalar и invert сделаны void методами. Изменена логика метода invert. Добавлен метод getComponent. Изменён метод equals.

--- Vector/src/ru/academits/oop/asher/vector_task/main/Main.java
+++ Vector/src/ru/academits/oop/asher/vector_task/main/Main.java
@@ -4,62 +4,66 @@ import ru.academits.oop.asher.vector_task.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        // Создание нового вектора vector1 с помощью первого конструктора
+        System.out.println("Создание нового вектора vector1 с помощью первого конструктора: ");
         Vector vector1 = new Vector(4);
         System.out.println(vector1.toString());
 
-        // Создание нового вектора vector2 с помощью копирования vector1
+        System.out.println("Создание нового вектора vector2 с помощью копирования vector1: ");
         Vector vector2 = new Vector(vector1);
         System.out.println(vector2.toString());
 
-        // Создание нового вектора vector3 с помощью третьего конструктора
-        double[] components = {2, 1, 3};
-        Vector vector3 = new Vector(components);
+        System.out.println("Создание нового вектора vector3 с помощью третьего конструктора: ");
+        double[] components3 = {2, 1, 3, 1};
+        Vector vector3 = new Vector(components3);
         System.out.println(vector3.toString());
 
-        // Создание нового вектора vector4 с помощью четвертого конструктора
-        Vector vector4 = new Vector(4, components);
+        System.out.println("Создание нового вектора vector4 с помощью четвертого конструктора: ");
+        double[] components4 = {0, 5, 2, 3};
+        Vector vector4 = new Vector(6, components4);
         System.out.println(vector4.toString());
 
-        // Получение размерности vector1
-        int size = vector1.getSize();
+        System.out.println("Получение размерности vector4: ");
+        int size = vector4.getSize();
         System.out.println(size);
 
-        // Сложение векторов статическим и нестатическим методами
+        System.out.println("Сложение векторов 3 и 4 нестатическим и статическим методами: ");
+        vector3.getSum(vector4);
+        System.out.println(vector3.toString());
+
         Vector vector5 = Vector.getSum(vector3, vector4);
         System.out.println(vector5.toString());
 
-        Vector vector6 = vector3.getSum(vector4);
+        System.out.println("Вычитание векторов 3 и 4 нестатическим и статическим методами: ");
+        vector3.getDifference(vector4);
+        System.out.println(vector3.toString());
+
+        Vector vector6 = Vector.getDifference(vector3, vector4);
         System.out.println(vector6.toString());
 
-        // Вычитание векторов статическим и нестатическим методами
-        Vector vector7 = Vector.getDifference(vector3, vector4);
-        System.out.println(vector7.toString());
-
-        Vector vector8 = vector3.getDifference(vector4);
-        System.out.println(vector8.toString());
-
-        // Нахождение скалярного произведения
+        System.out.println("Нахождение скалярного произведения векторов 3 и 4: ");
         double dotProduct = Vector.getDotProduct(vector3, vector4);
         System.out.println(dotProduct);
 
-        // Умножение вектора на скаляр
-        Vector vector9 = vector3.multiplyByScalar(2);
-        System.out.println(vector9.toString());
+        System.out.println("Умножение вектора 3 на скаляр 2: ");
+        vector3.multiplyByScalar(2);
+        System.out.println(vector3.toString());
 
-        // Разворот вектора
-        Vector vector10 = vector9.expand();
-        System.out.println(vector10.toString());
+        System.out.println("Разворот вектора 3: ");
+        vector3.invert();
+        System.out.println(vector3.toString());
 
-        // Получение длины вектора
+        System.out.println("Получение длины вектора 3: ");
         double length = vector3.getLength();
         System.out.println(length);
 
-        // Получение и установка компоненты вектора по индексу
+        System.out.println("Получение компоненты вектора по индексу: ");
+        System.out.println(vector1.getComponent(2));
+
+        System.out.println("Установка компоненты вектора по индексу: ");
         vector1.setComponent(2, 1);
         System.out.println(vector1.toString());
 
-        // Попытка создать вектор с отрицательной размерностью
+        System.out.println("Попытка создать вектор с отрицательной размерностью: ");
         Vector vector11 = new Vector(-2);
         System.out.println(vector11.toString());
     }
--- Vector/src/ru/academits/oop/asher/vector_task/vector/Vector.java
+++ Vector/src/ru/academits/oop/asher/vector_task/vector/Vector.java
@@ -5,56 +5,37 @@ import java.util.Arrays;
 public class Vector {
     private double[] components;
 
-    // Первый конструктор, принимающий размерность n
-    public Vector(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("Размерность вектора должна быть больше нуля.");
-        }
-        this.components = new double[n];
-    }
-
-    // Второй конструктор - конструктор копирования
-    public Vector(Vector vector) {
-        this.components = vector.components;
-    }
-
-    // Третий конструктор, принимающий массив components
-    public Vector(double[] components) {
-        this.components = components;
-    }
-
-    // Четвёртый конструктор, принимающий размерность n и массив components
-    public Vector(int n, double[] components) {
-        if (n <= 0) {
+    // Первый конструктор, принимающий размерность "dimension"
+    public Vector(int dimension) {
+        if (dimension <= 0) {
             throw new IllegalArgumentException("Размерность вектора должна быть больше нуля.");
         }
 
-        if (n == components.length) {
-            this.components = components;
-        } else {
-            double[] newComponents = new double[n];
-            for (int i = 0; i < components.length; i++) {
-                newComponents[i] = components[i];
-            }
-            for (int i = components.length; i < n; i++) {
-                newComponents[i] = 0;
-            }
-            this.components = newComponents;
-        }
-    }
-
-    // Геттеры и сеттеры для поля components
-    public double[] getComponents() {
-        return components;
-    }
-
-    public void setComponents(double[] components) {
-        this.components = components;
+        this.components = new double[dimension];
     }
 
     // Метод для получения размерности вектора
     public int getSize() {
         return components.length;
+    }
+
+    // Второй конструктор - конструктор копирования
+    public Vector(Vector vector) {
+        this.components = Arrays.copyOf(vector.components, vector.getSize());
+    }
+
+    // Третий конструктор, принимающий массив components
+    public Vector(double[] components) {
+        this.components = Arrays.copyOf(components, components.length);
+    }
+
+    // Четвёртый конструктор, принимающий размерность n и массив components
+    public Vector(int dimension, double[] components) {
+        if (dimension <= 0) {
+            throw new IllegalArgumentException("Размерность вектора должна быть больше нуля.");
+        }
+
+        this.components = Arrays.copyOf(components, dimension);
     }
 
     // Переопределение метода toString
@@ -73,132 +54,36 @@ public class Vector {
         return stringBuilder.toString();
     }
 
-    // Статический метод для сложения векторов
-    public static Vector getSum(Vector vector1, Vector vector2) {
-        if (vector1.components.length == vector2.components.length) {
-            double[] newComponents = new double[vector1.components.length];
+    // Нестатический метод для сложения векторов (this + vector)
+    public void getSum(Vector vector) {
+        int maxLength = Math.max(this.components.length, vector.components.length);
+        this.components = Arrays.copyOf(this.components, maxLength);
 
-            for (int i = 0; i < vector1.components.length; i++) {
-                newComponents[i] = vector1.components[i] + vector2.components[i];
-            }
-
-            return new Vector(newComponents);
+        for (int i = 0; i < vector.components.length; i++) {
+            this.components[i] += vector.components[i];
         }
-
-        int maxLength = Math.max(vector1.components.length, vector2.components.length);
-        int minLength = Math.min(vector1.components.length, vector2.components.length);
-
-        double[] newComponents = new double[maxLength];
-
-        for (int i = 0; i < minLength; i++) {
-            newComponents[i] = vector1.components[i] + vector2.components[i];
-        }
-
-        for (int i = minLength; i < maxLength; i++) {
-            if (vector1.components.length == maxLength) {
-                newComponents[i] = vector1.components[i];
-            } else {
-                newComponents[i] = vector2.components[i];
-            }
-        }
-
-        return new Vector(newComponents);
     }
 
-    // Нестатический метод для сложения векторов
-    public Vector getSum(Vector vector) {
-        if (this.components.length == vector.components.length) {
-            double[] newComponents = new double[this.components.length];
+    // Статический метод для сложения векторов
+    public static Vector getSum(Vector vector1, Vector vector2) {
+        vector1.getSum(vector2);
+        return new Vector(Arrays.copyOf(vector1.components, vector1.components.length));
+    }
 
-            for (int i = 0; i < this.components.length; i++) {
-                newComponents[i] = this.components[i] + vector.components[i];
-            }
-
-            return new Vector(newComponents);
-        }
-
+    // Нестатический метод для вычитания векторов (this - vector)
+    public void getDifference(Vector vector) {
         int maxLength = Math.max(this.components.length, vector.components.length);
-        int minLength = Math.min(this.components.length, vector.components.length);
+        this.components = Arrays.copyOf(this.components, maxLength);
 
-        double[] newComponents = new double[maxLength];
-
-        for (int i = 0; i < minLength; i++) {
-            newComponents[i] = this.components[i] + vector.components[i];
+        for (int i = 0; i < vector.components.length; i++) {
+            this.components[i] -= vector.components[i];
         }
-
-        for (int i = minLength; i < maxLength; i++) {
-            if (this.components.length == maxLength) {
-                newComponents[i] = this.components[i];
-            } else {
-                newComponents[i] = vector.components[i];
-            }
-        }
-
-        return new Vector(newComponents);
     }
 
     // Статический метод для вычитания векторов (vector1 - vector2)
     public static Vector getDifference(Vector vector1, Vector vector2) {
-        if (vector1.components.length == vector2.components.length) {
-            double[] newComponents = new double[vector1.components.length];
-
-            for (int i = 0; i < vector1.components.length; i++) {
-                newComponents[i] = vector1.components[i] - vector2.components[i];
-            }
-
-            return new Vector(newComponents);
-        }
-
-        int maxLength = Math.max(vector1.components.length, vector2.components.length);
-        int minLength = Math.min(vector1.components.length, vector2.components.length);
-
-        double[] newComponents = new double[maxLength];
-
-        for (int i = 0; i < minLength; i++) {
-            newComponents[i] = vector1.components[i] - vector2.components[i];
-        }
-
-        for (int i = minLength; i < maxLength; i++) {
-            if (vector1.components.length == maxLength) {
-                newComponents[i] = vector1.components[i];
-            } else {
-                newComponents[i] = 0 - vector2.components[i];
-            }
-        }
-
-        return new Vector(newComponents);
-    }
-
-    // Нестатический метод для вычитания векторов (this - vector)
-    public Vector getDifference(Vector vector) {
-        if (this.components.length == vector.components.length) {
-            double[] newComponents = new double[this.components.length];
-
-            for (int i = 0; i < this.components.length; i++) {
-                newComponents[i] = this.components[i] - vector.components[i];
-            }
-
-            return new Vector(newComponents);
-        }
-
-        int maxLength = Math.max(this.components.length, vector.components.length);
-        int minLength = Math.min(this.components.length, vector.components.length);
-
-        double[] newComponents = new double[maxLength];
-
-        for (int i = 0; i < minLength; i++) {
-            newComponents[i] = this.components[i] - vector.components[i];
-        }
-
-        for (int i = minLength; i < maxLength; i++) {
-            if (this.components.length == maxLength) {
-                newComponents[i] = this.components[i];
-            } else {
-                newComponents[i] = 0 - vector.components[i];
-            }
-        }
-
-        return new Vector(newComponents);
+        vector1.getDifference(vector2);
+        return new Vector(Arrays.copyOf(vector1.components, vector1.components.length));
     }
 
     // Статический метод для нахождения скалярного произведения ("dot product")
@@ -214,25 +99,15 @@ public class Vector {
     }
 
     // Нестатический метод для умножения вектора на скаляр
-    public Vector multiplyByScalar(double scalar) {
-        double[] newComponents = new double[this.components.length];
-
+    public void multiplyByScalar(double scalar) {
         for (int i = 0; i < this.components.length; i++) {
-            newComponents[i] = this.components[i] * scalar;
+            this.components[i] = this.components[i] * scalar;
         }
-
-        return new Vector(newComponents);
     }
 
     // Нестатический метод для разворота вектора
-    public Vector expand() {
-        double[] newComponents = new double[this.components.length];
-
-        for (int i = 0; i < this.components.length; i++) {
-            newComponents[i] = this.components[i] * -1;
-        }
-
-        return new Vector(newComponents);
+    public void invert() {
+        this.multiplyByScalar(-1);
     }
 
     // Нестатический метод для получения длины вектора
@@ -246,17 +121,19 @@ public class Vector {
         return Math.sqrt(vectorLength);
     }
 
-    // Нестатический метод для получения и установки компоненты вектора по индексу
-    public void setComponent(double component, int index) {
+    // Нестатический метод для получения компоненты вектора по индексу
+    public double getComponent(int index) {
+        return this.components[index];
+    }
+
+    // Нестатический метод для установки компоненты вектора по индексу
+    public void setComponent(int index, double component) {
         this.components[index] = component;
     }
 
     // Переопределение метода equals
     @Override
     public boolean equals(Object object) {
-        if (object == this) {
-            return true;
-        }
         if (object == null || object.getClass() != this.getClass()) {
             return false;
         }
