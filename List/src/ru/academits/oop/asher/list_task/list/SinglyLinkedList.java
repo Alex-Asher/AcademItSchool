01d835f3497eaf5edf78f8e52183a6cecc1b7fae
Alex-Asher <AA07101994@gmail.com>
2020-05-02 22:22:26 +0700

    В задаче List удалён конструктор без аргументов. Доработаны исключения и проверки. Многие методы оптимизированы: в них больше не используется метод findElement.

--- List/src/ru/academits/oop/asher/list_task/list/SinglyLinkedList.java
+++ List/src/ru/academits/oop/asher/list_task/list/SinglyLinkedList.java
@@ -1,13 +1,11 @@
 package ru.academits.oop.asher.list_task.list;
 
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class SinglyLinkedList<T> {
     private ListItem<T> head;
     private int count;
-
-    public SinglyLinkedList() {
-    }
 
     // Получение размера списка
     public int getCount() {
@@ -17,7 +15,7 @@ public class SinglyLinkedList<T> {
     // Получение значения первого элемента
     public T getHead() {
         if (count == 0) {
-            throw new IllegalArgumentException("Вызов метода невозможен для пустого списка.");
+            throw new NoSuchElementException("Вызов метода невозможен для пустого списка.");
         }
 
         return head.getData();
@@ -38,49 +36,41 @@ public class SinglyLinkedList<T> {
 
     // Вспомогательный метод: получение узла по индексу
     private ListItem<T> findElement(int index) {
-        if (index < 0) {
-            throw new IndexOutOfBoundsException("Индекс не может быть отрицательным числом.");
+        if (index < 0 || index >= count) {
+            throw new IndexOutOfBoundsException("Индекс выходит за границы списка.");
         }
 
         ListItem<T> element = head;
+
         for (int i = 0; i < index; i++) {
             element = element.getNext();
         }
+
         return element;
     }
 
     // Получение значения по указанному индексу
     public T getValue(int index) {
-        if (index < 0) {
-            throw new IndexOutOfBoundsException("Индекс не может быть отрицательным числом.");
-        }
-
         return findElement(index).getData();
     }
 
     // Изменение значения по указанному индексу. Возвращается старое значение
     public T setValue(int index, T data) {
-        if (index < 0) {
-            throw new IndexOutOfBoundsException("Индекс не может быть отрицательным числом.");
-        }
+        ListItem<T> element = findElement(index);
+        T oldData = element.getData();
+        element.setData(data);
 
-        T oldData = findElement(index).getData();
-        findElement(index).setData(data);
         return oldData;
     }
 
     // Удаление элемента по индексу. Возвращается значение элемента
     public T deleteElement(int index) {
-        if (index < 0) {
-            throw new IndexOutOfBoundsException("Индекс не может быть отрицательным числом.");
-        }
-
         if (index == 0) {
             return deleteFirst();
         }
 
         ListItem<T> previousElement = findElement(index - 1);
-        ListItem<T> element = findElement(index);
+        ListItem<T> element = previousElement.getNext();
         ListItem<T> nextElement = element.getNext();
 
         previousElement.setNext(nextElement);
@@ -96,10 +86,6 @@ public class SinglyLinkedList<T> {
 
     // Вставка элемента по индексу
     public void insertElement(int index, T data) {
-        if (index < 0) {
-            throw new IndexOutOfBoundsException("Индекс не может быть отрицательным числом.");
-        }
-
         ListItem<T> previousElement = findElement(index - 1);
         ListItem<T> element = new ListItem<>(data);
         ListItem<T> nextElement = previousElement.getNext();
@@ -116,22 +102,29 @@ public class SinglyLinkedList<T> {
             return true;
         }
 
+        ListItem<T> previousElement = head;
         ListItem<T> element = head.getNext();
+        ListItem<T> nextElement = element.getNext();
+
         for (int i = 1; i < count; i++) {
             if (Objects.equals(element.getData(), data)) {
-                findElement(i - 1).setNext(findElement(i + 1));
+                previousElement.setNext(nextElement);
                 count--;
                 return true;
             }
+
+            previousElement = previousElement.getNext();
             element = element.getNext();
+            nextElement = nextElement.getNext();
         }
+
         return false;
     }
 
     // Удаление первого элемента. Возвращает значение элемента
     public T deleteFirst() {
         if (count == 0) {
-            throw new IllegalArgumentException("Вызов метода невозможен для пустого списка.");
+            throw new NoSuchElementException("Вызов метода невозможен для пустого списка.");
         }
 
         ListItem<T> oldHead = head;
@@ -143,7 +136,7 @@ public class SinglyLinkedList<T> {
     // Разворот списка за линейное время
     public void reverse() {
         if (count == 0) {
-            throw new IllegalArgumentException("Вызов метода невозможен для пустого списка.");
+            return;
         }
 
         ListItem<T> previousElement = null;
@@ -161,15 +154,18 @@ public class SinglyLinkedList<T> {
         head = element;
     }
 
+    // Копирование списка
     public SinglyLinkedList<T> copy() {
         if (count == 0) {
-            throw new IllegalArgumentException("Вызов метода невозможен для пустого списка.");
+            new SinglyLinkedList<>();
         }
 
         SinglyLinkedList<T> copy = new SinglyLinkedList<>();
+        ListItem<T> element = head;
 
         for (int i = 0; i < count; i++) {
-            copy.add(findElement(i).getData());
+            copy.add(element.getData());
+            element = element.getNext();
         }
 
         copy.count = count;
@@ -179,15 +175,21 @@ public class SinglyLinkedList<T> {
     // Вспомогательный метод: переопределение toString
     @Override
     public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("{");
-
-        for (int i = 0; i < count - 1; i++) {
-            stringBuilder.append(findElement(i).getData())
-                    .append(", ");
+        if (count == 0) {
+            return ("Список пуст.");
         }
 
-        stringBuilder.append(findElement(count - 1).getData())
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{");
+        ListItem<T> element = head;
+
+        for (int i = 0; i < count - 1; i++) {
+            stringBuilder.append(element.getData())
+                    .append(", ");
+            element = element.getNext();
+        }
+
+        stringBuilder.append(element.getData())
                 .append("}");
 
         return stringBuilder.toString();
